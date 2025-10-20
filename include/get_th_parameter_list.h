f4dc1b75ebcced74f8f92381e166989e53084c49
xyzhou <kirito2357@mail.ustc.edu.cn>
2025-10-20 13:28:17 +0800

    modified:   include/get_th_parameter_list.h modified:   src/get_th_parameter_list.cxx modified:   src/main.cxx

--- include/get_th_parameter_list.h
+++ include/get_th_parameter_list.h
@@ -4,4 +4,4 @@
 #include "bin.h"
 #include "th_parameter.h"
 
-std::vector<th_parameter> get_th_parameter_list(const std::string &out_file, std::vector<const char *> &sineff_test, std::vector<bin> Mass_bin_list);
+std::vector<th_parameter> get_th_parameter_list(const std::string &out_file, std::vector<const char *> sineff_test, std::vector<bin> Mass_bin_list);
